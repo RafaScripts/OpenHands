9377a7b20e4d5e4e5d274e81a7be707929689c33
Rafael <rafaelfreire0@gmail.com>
2025-12-24 17:58:56 -0300

    testing

--- openhands/app_server/sandbox/docker_sandbox_service.py
+++ openhands/app_server/sandbox/docker_sandbox_service.py
@@ -346,6 +346,7 @@ class DockerSandboxService(SandboxService):
                 working_dir=sandbox_spec.working_dir,
                 labels=labels,
                 detach=True,
+                extra_hosts={'host.docker.internal': 'host-gateway'},  # type: ignore[arg-type]
             )
 
             sandbox_info = await self._container_to_sandbox_info(container)
